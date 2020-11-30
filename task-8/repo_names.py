43c4cf2a051a2ff1d412569451fba6db98db4319
R Adithya Kumar <73246484+SilentCruzer@users.noreply.github.com>
2020-11-30 20:13:44 +0530

    Update repo_names.py

--- task-8/repo_names.py
+++ task-8/repo_names.py
@@ -1,15 +1,14 @@
 from github import Github
 import subprocess
 import time
-
-username = "amfoss"
+	
+username = input("Enter username: ")
 user = Github().get_user(username)
 print("Fetching repository names and commits\nIt may take a few seconds.....")
-
 for repo in user.get_repos():	
 	with open('commits.json', 'a') as file:
 		file.write("\nCommits of Repository " + repo.name + " :\n" )
-	subprocess.check_output("perceval git --json-line https://github.com/amfoss/" + repo.name +">>commits.json", shell = True, stderr=subprocess.STDOUT)
+	subprocess.check_output("perceval git --json-line https://github.com/"+username+"/" + repo.name +">>commits.json", shell = True, stderr=subprocess.STDOUT)
 	time.sleep(5)
 
 print("Finished, data can be seen in commits.json in your directory")
